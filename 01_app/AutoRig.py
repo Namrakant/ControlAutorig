f315b25724a3756b1017dbb712283ee3e6027f19
Namrakant <namrakant23@gmail.com>
2024-02-10 23:58:28 -0500

    INIT 01_app

---
+++ 01_app/AutoRig.py
@@ -0,0 +1,14 @@
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
+# Title: Control Autorigger
+# Version: 0.1
+# Description: This is a sample script to start this project. 
+#              This script will be able to identify the joint hierarchy of a 
+#              humanoid character and create control, FK and IK on top of the joints.
+#              This will also have a UI
+# Creation Date: 02/10/2024
+# Author: Namrakant Tamrakar
+#---------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------#
+
+print("This is a control autorigger")
