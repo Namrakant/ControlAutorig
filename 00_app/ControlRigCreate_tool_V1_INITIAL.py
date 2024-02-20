73032dd3cd9111d95227694a4dc5792fba21719a
Namrakant <namrakant23@gmail.com>
2024-02-20 11:46:40 -0500

    Added initial working UI, for FK only

--- 00_app/ControlRigCreate_tool_V1_INITIAL.py
+++ 00_app/ControlRigCreate_tool_V1_INITIAL.py
@@ -8,12 +8,216 @@
 #                Manually resize the controls based on your needs. Controls are 
 #                color coded.
 #                Note: Select root joint first!                
-# Creation Date: 01/10/2024
+# Creation Date: 02/20/2024
 # Author       : Namrakant Tamrakar
 #---------------------------------------------------------------------------------#
 #---------------------------------------------------------------------------------#
-
+import os
 import maya.cmds as mc
+import maya.mel as mel
+from functools import partial
+import maya.OpenMayaUI as omui
+
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+from shiboken2 import wrapInstance
+
+#---------------------------------------------------------------------------------#
+# Function to keep the User Interface on top of Maya
+#---------------------------------------------------------------------------------#
+def maya_main_window():
+    """
+    Return the Maya main window widget as a Python object
+    """
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    return wrapInstance(int(main_window_ptr), QtWidgets.QWidget)
+
+#---------------------------------------------------------------------------------#
+# Class to smooth the animation on the FBX in the scene
+#---------------------------------------------------------------------------------#
+class ControlAutorig(QtWidgets.QDialog):
+
+    def __init__(self, parent=maya_main_window()):
+        """
+        Constructor used to call all the methods
+        """
+        super(ControlAutorig, self).__init__(parent)
+
+        self.setWindowTitle("Control Autorig Tool")
+        self.setMinimumWidth(500)
+
+        self.create_widgets()
+        self.create_layouts()
+        self.create_connections()
+
+    def create_widgets(self):
+        """
+        This method creates widgets such as radiobutton, lineedit and button
+        """
+        self.rootJointLineedit = QtWidgets.QLineEdit("example- main_jnt")
+        self.rootJointBtn = QtWidgets.QPushButton("Select Root Joint")
+
+        self.hipJointLineedit = QtWidgets.QLineEdit("example- l_hip_jnt")
+        self.hipJointBtn = QtWidgets.QPushButton("Select Hip Joint")
+
+        self.shoulderJointLineedit = QtWidgets.QLineEdit("example- l_shoulder_jnt")
+        self.shoulderJointBtn = QtWidgets.QPushButton("Select Shoulder Joint")
+        
+        self.select_FK = QtWidgets.QRadioButton('FK Controls only')
+        self.select_FK.setChecked(True)
+        self.select_FKIK = QtWidgets.QRadioButton('FK/IK Controls')
+
+        self.createControlsBtn = QtWidgets.QPushButton("Create Controls")
+
+    def create_layouts(self):
+        """
+        This method creates the layout to hold the widgets. 
+        Without this, you won't see the widgets you have created in Maya.
+        """
+
+        FKIK_rb_layout = QtWidgets.QHBoxLayout()
+        FKIK_rb_layout.addWidget(self.select_FK)
+        FKIK_rb_layout.addWidget(self.select_FKIK)
+
+        root_button_layout = QtWidgets.QHBoxLayout()
+        # root_button_layout.addStretch()
+        root_button_layout.addWidget(self.rootJointLineedit)
+        root_button_layout.addWidget(self.rootJointBtn)
+        
+        hip_button_layout = QtWidgets.QHBoxLayout()
+        # hip_button_layout.addStretch()
+        hip_button_layout.addWidget(self.hipJointLineedit)
+        hip_button_layout.addWidget(self.hipJointBtn)
+
+        shoulder_button_layout = QtWidgets.QHBoxLayout()
+        # shoulder_button_layout.addStretch()
+        shoulder_button_layout.addWidget(self.shoulderJointLineedit)
+        shoulder_button_layout.addWidget(self.shoulderJointBtn)
+
+        form_layout = QtWidgets.QFormLayout()
+        # form_layout.addRow("Select all joints", self.selectAllJoints)
+        form_layout.addRow("Select root joint and Press", root_button_layout)
+        form_layout.addRow("Select hip joint and Press", hip_button_layout)
+        form_layout.addRow("Select shoulder joint and Press", shoulder_button_layout)
+
+        form_layout.addRow("", FKIK_rb_layout)
+        form_layout.addRow("", self.createControlsBtn)
+        
+        # Main Layout
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addLayout(form_layout)
+
+#------------------------------- CREATE CONNECTIONS METHOD --------------------------#
+    def create_connections(self):
+        """
+        This method connects the buttons to its respective methods
+        """
+        self.rootJointBtn.clicked.connect(self.rootJointMethod)
+            
+        self.hipJointBtn.clicked.connect(self.hipJointMethod)
+        # if self.hipJointName != "":
+        #     self.hipJointLineedit.setText(self.hipJointName)
+
+        self.shoulderJointBtn.clicked.connect(self.shoulderJointMethod)
+        self.createControlsBtn.clicked.connect(self.createControlsMethod)
+        # if self.shoulderJointName != "":
+        #     self.shoulderJointLineedit.setText(self.shoulderJointName)
+        # self.weightedTangents.clicked.connect(self.convertToWeightedTangents)
+
+        # Get the filepath if it already has one
+        # fullfilepath = mc.file(q=True, sn=True)
+        # # if self.save_playblast_path:
+        # #     self.playblastFilepath.setText(self.save_playblast_path)
+        # if fullfilepath:
+        #     self.filepath = os.path.split(fullfilepath)[0]
+        #     self.filename = os.path.split(fullfilepath)[1].split('.')[0]
+
+        #     self.playblastFilepath.setText(self.filepath)
+        #     self.playblastFilename.setText(self.filename)
+
+        #     self.exportFBXFilepath.setText(self.filepath)
+        #     self.exportFBXFilename.setText(self.filename)
+
+        #     # self.playblastFilepath.editingFinished.connect(self.filepath)
+        #     # self.playblastFilename.editingFinished.connect(self.filename)
+        # # self.numberOfIteration.editingFinished.connect(self.numberOfIterations)
+        # self.smooth.clicked.connect(self.smoothCurve)
+
+    def rootJointMethod(self):
+        self.rootJointName = mc.ls(sl=True, type='joint') # Do a check if any other joint exist above it or not
+        print(self.rootJointName)
+        if self.rootJointName != []:
+            self.rootJointLineedit.setText(self.rootJointName[0])
+            print(type(self.rootJointName), self.rootJointName)
+        else:
+            print("Please select root joint")
+        # return str(self.rootJointName)
+    
+    def hipJointMethod(self):
+        self.hipJointName = mc.ls(sl=True, type='joint') # Do a check if any other joint exist above it or not
+        print(self.hipJointName)
+        if self.hipJointName != []:
+            self.hipJointLineedit.setText(self.hipJointName[0])
+            print(type(self.hipJointName), self.hipJointName)
+        else:
+            print("Please select hip joint")
+    
+    def shoulderJointMethod(self):
+        self.shoulderJointName = mc.ls(sl=True, type='joint') # Do a check if any other joint exist above it or not
+        print(self.shoulderJointName)
+        if self.shoulderJointName != []:
+            self.shoulderJointLineedit.setText(self.shoulderJointName[0])
+            print(type(self.shoulderJointName), self.shoulderJointName)
+        else:
+            print("Please select shoulder joint")
+    
+    def createControlsMethod(self):
+        #-------------------------------------------------------------------------------#
+        #-------------------- CALL CONTROL CREATION FUNCTION ---------------------------#
+        #-------------------------------------------------------------------------------#
+
+        # select joints
+        selected_joints = mc.ls(sl=True, type='joint')
+        # selected_joints = mc.listRelatives('m_avg_root', ad=True, type='joint') # this doesn't work because of sequence order
+        # temp_circle = mc.circle( n = 'temp_ctrl', ch = False, normal = [0,1,0], radius = 1.0 )[0] #ch = channel history
+        # mc.setAttr(str(temp_circle)+ '.translateY', 200)
+
+        for jnt in selected_joints:
+            # get the name for control
+            ctrl_name_prefix = jnt.split("m_avg_")[-1]
+            print()
+            print('-----------------------------------------')
+            # print(ctrl_name_prefix)
+
+            # identify rotateTo joint, basically the child joint
+            
+            # rotateTo_jnt = find_child(jnt)
+            # print("Rotate To for " + jnt + " joint = " + str(rotateTo_jnt))
+
+            # create controls. passing below parameters -
+                        # prefix = 'new', 
+                        # scale = 1.0, 
+                        # translateTo = '',
+                        # rotateTo = '',
+                        # parent = '',
+                        # shape = 'circle',
+                        # lockChannels = ['s','v']
+            
+            ctrl_name, ctrlOffsetName = create_controls( prefix=ctrl_name_prefix, scale=20.0, 
+                            translateTo=jnt, rotateTo=jnt, 
+                            parent="", shape='circleY', lockChannels=[])
+            
+            # find the parent and parent the offset control to its parent control similar to joint hierarchy
+            parent_of_jnt = find_parent(jnt)
+
+            # parent respective controllers hierarchy, similar to joint hierarchy
+            controller_hierarchy(parent_of_jnt, ctrlOffsetName)    
+            
+            # parent constraint the controllers to respective joint
+            parent_constraint_control_to_jnt(ctrl_name, jnt)
+
+
+        # mc.delete(temp_circle)
 
 prefix = 'new'
 scale = 1.0
@@ -193,49 +397,64 @@ def controller_hierarchy(parent_of_jnt, ctrlOffsetName):
         parent_of_jnt_prefix = parent_of_jnt.split("m_avg_")[-1]    
         mc.parent(ctrlOffsetName, parent_of_jnt_prefix + '_ctrl')
 
-#-------------------------------------------------------------------------------#
-#-------------------- CALL CONTROL CREATION FUNCTION ---------------------------#
-#-------------------------------------------------------------------------------#
+# #-------------------------------------------------------------------------------#
+# #-------------------- CALL CONTROL CREATION FUNCTION ---------------------------#
+# #-------------------------------------------------------------------------------#
 
-# select joints
-selected_joints = mc.ls(sl=True, type='joint')
-# selected_joints = mc.listRelatives('m_avg_root', ad=True, type='joint') # this doesn't work because of sequence order
-# temp_circle = mc.circle( n = 'temp_ctrl', ch = False, normal = [0,1,0], radius = 1.0 )[0] #ch = channel history
-# mc.setAttr(str(temp_circle)+ '.translateY', 200)
+# # select joints
+# selected_joints = mc.ls(sl=True, type='joint')
+# # selected_joints = mc.listRelatives('m_avg_root', ad=True, type='joint') # this doesn't work because of sequence order
+# # temp_circle = mc.circle( n = 'temp_ctrl', ch = False, normal = [0,1,0], radius = 1.0 )[0] #ch = channel history
+# # mc.setAttr(str(temp_circle)+ '.translateY', 200)
 
-for jnt in selected_joints:
-    # get the name for control
-    ctrl_name_prefix = jnt.split("m_avg_")[-1]
-    print()
-    print('-----------------------------------------')
-    # print(ctrl_name_prefix)
+# for jnt in selected_joints:
+#     # get the name for control
+#     ctrl_name_prefix = jnt.split("m_avg_")[-1]
+#     print()
+#     print('-----------------------------------------')
+#     # print(ctrl_name_prefix)
 
-    # identify rotateTo joint, basically the child joint
+#     # identify rotateTo joint, basically the child joint
     
-    # rotateTo_jnt = find_child(jnt)
-    # print("Rotate To for " + jnt + " joint = " + str(rotateTo_jnt))
+#     # rotateTo_jnt = find_child(jnt)
+#     # print("Rotate To for " + jnt + " joint = " + str(rotateTo_jnt))
 
-    # create controls. passing below parameters -
-                # prefix = 'new', 
-                # scale = 1.0, 
-                # translateTo = '',
-                # rotateTo = '',
-                # parent = '',
-                # shape = 'circle',
-                # lockChannels = ['s','v']
+#     # create controls. passing below parameters -
+#                 # prefix = 'new', 
+#                 # scale = 1.0, 
+#                 # translateTo = '',
+#                 # rotateTo = '',
+#                 # parent = '',
+#                 # shape = 'circle',
+#                 # lockChannels = ['s','v']
     
-    ctrl_name, ctrlOffsetName = create_controls( prefix=ctrl_name_prefix, scale=20.0, 
-                    translateTo=jnt, rotateTo=jnt, 
-                    parent="", shape='circleY', lockChannels=[])
+#     ctrl_name, ctrlOffsetName = create_controls( prefix=ctrl_name_prefix, scale=20.0, 
+#                     translateTo=jnt, rotateTo=jnt, 
+#                     parent="", shape='circleY', lockChannels=[])
     
-    # find the parent and parent the offset control to its parent control similar to joint hierarchy
-    parent_of_jnt = find_parent(jnt)
+#     # find the parent and parent the offset control to its parent control similar to joint hierarchy
+#     parent_of_jnt = find_parent(jnt)
 
-    # parent respective controllers hierarchy, similar to joint hierarchy
-    controller_hierarchy(parent_of_jnt, ctrlOffsetName)    
+#     # parent respective controllers hierarchy, similar to joint hierarchy
+#     controller_hierarchy(parent_of_jnt, ctrlOffsetName)    
     
-    # parent constraint the controllers to respective joint
-    parent_constraint_control_to_jnt(ctrl_name, jnt)
+#     # parent constraint the controllers to respective joint
+#     parent_constraint_control_to_jnt(ctrl_name, jnt)
 
 
 # mc.delete(temp_circle)
+    
+
+#---------------------------------------------------------------------------------#
+#                 CALLING THE CONTROL AUTORIG TOOL
+#---------------------------------------------------------------------------------#
+if __name__ == "__main__":
+
+    try:
+        ControlAutorig.close(ControlAutorigUI)
+        ControlAutorig.deleteLater()
+    except:
+        pass
+
+    ControlAutorigUI = ControlAutorig()
+    ControlAutorigUI.show()
