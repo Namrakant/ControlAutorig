f2192bd926e8137c809396f0a5141448dc31ea9b
Namrakant <namrakant23@gmail.com>
2024-03-19 12:40:54 -0400

    Transfer animation tool is working

--- 04_ui/ControlRigCreate_tool_V1.py
+++ 04_ui/ControlRigCreate_tool_V1.py
@@ -14,7 +14,6 @@
 #---------------------------------------------------------------------------------#
 
 import os
-# import maya.cmds as mc          # ModuleNotFoundError: No module named 'maya' if run on OS. Moved inside main call.
 
 from Qt.QtWidgets import QMessageBox, QFileDialog
 from Qt import QtWidgets, QtGui, QtCore, QtCompat
@@ -23,7 +22,7 @@ from Qt import QtWidgets, QtGui, QtCore, QtCompat
 # Class to create control autorig UI by loading the ControlAutorig.ui File
 #---------------------------------------------------------------------------------#
 class ControlAutorig(QtWidgets.QDialog):
-    def __init__(self):
+    def __init__(self, path_ui):
         # LOAD ui with absolute path
         self.wgUtil = QtCompat.loadUi(path_ui)
         
@@ -53,29 +52,67 @@ class ControlAutorig(QtWidgets.QDialog):
 
     def referenceAnimationMethod(self):
         filePath, fileType = QFileDialog.getOpenFileName(None, "Select File", "", "All Files (*.*)")
-        mc.file(filePath, reference=True, namespace="myNamespace")
+        mc.file(filePath, reference=True, namespace="AnimTransfer")
 
     def deleteReference(self):        
-        referencedFile = mc.file(query=True, reference=True)[0]
-        # Get the reference node of the referenced file
-        referenceNodes = mc.referenceQuery(referencedFile, referenceNode=True)
-        # # Remove the reference
-        mc.file(removeReference=True, referenceNode=referenceNodes)
-        print("Reference Deleted")
+        try:
+            referencedFile = mc.file(query=True, reference=True)[0]
+            # Get the reference node of the referenced file
+            referenceNodes = mc.referenceQuery(referencedFile, referenceNode=True)
+            # # Remove the reference
+            mc.file(removeReference=True, referenceNode=referenceNodes)
+            print("Reference Deleted")
+        except:
+            QMessageBox.critical(None, 'Critical', 'There is no reference to be deleted')
 
     def transferAnimationMethod(self):
         # transfer animation from joints to controls
         print("Transfer animation is work in progress")
 
+        # initiating variables
+        self.source_keys = []
+        self.target_keys = []
+
+        self.source_keys = mc.ls('AnimTransfer:*:*', type='joint')
+        self.target_keys = mc.ls('*Ctrl', type='transform')
+
+        print('source_keys = ', self.source_keys)
+        print('target_keys = ', self.target_keys)
+
+        self.sourcePrefix = self.identifyPrefixMethod(self.source_keys[0])
+        
+        # map mixamo joints to new controls and copy-paste the keys
+        self.mapping(self.source_keys, self.target_keys)
+
         # Remove reference if checkbox is selected
-        if self.wgUtil.chkDeleteReference.isChecked() == True:
+        if self.wgUtil.chkDeleteReference.isChecked() == True:            
             self.deleteReference()
 
-    def identifyPrefixMethod(self):
-        try:
-            self.prefix = mc.ls(sl=1)[0].split("_")[0]
+    def mapping(self, source_keys, target_keys):
+
+        for source in source_keys:
+            sourceJnt = source.split(self.sourcePrefix)[-1]
+            for target in target_keys:
+                targetCtrl = target.split('_Ctrl')[0]
+                if  sourceJnt == targetCtrl:
+                    self.copyPasteKeys( source, target )
+
+    def copyPasteKeys(self, source, target):
+        anim = mc.copyKey( source )
+        if anim != 0: # anim is zero when there is no animation key to copy. No need to show any message
+            mc.pasteKey( target )
+
+    def identifyPrefixMethod(self, selection):
+        print('identifyPrefixMethod(self, selection):', selection)
+        try:            
+            if ":" in selection:
+                notPrefix = selection.split(':')[-1]
+            prefix = selection.split(str(notPrefix))[0]
+            print('prefix = ', prefix)
+            return prefix
         except:
-            QMessageBox.critical(None, 'Critical', 'Please select the root joint')
+            # QMessageBox.critical(None, 'Critical', 'Please select the root joint')
+            print('Please select the root joint')
 
     def rootJointMethod(self): # used in Connection
         self.rootJointName = mc.ls(sl=True, type='joint') # Do a check if any other joint exist above it or not
@@ -103,17 +140,16 @@ class ControlAutorig(QtWidgets.QDialog):
     
     #-------------------- Create FK Controls Method ---------------------------#
     def createControlsMethod(self): # used in Connection
-        
-        self.identifyPrefixMethod()
 
         # select all joints. Root joint should be the first joint
         selected_root_joint = mc.ls(sl=True, type='joint')
+        self.prefix = self.identifyPrefixMethod(selected_root_joint[0])
 
         for jnt in selected_root_joint:
             # get the name for control
             ctrl_name_prefix = jnt.split(self.prefix)[-1]
 
-            ctrl_name, ctrlOffsetName = self.createControls( prefix=ctrl_name_prefix, scale=20.0, 
+            ctrl_name, ctrlOffsetName = self.createControls( prefix=ctrl_name_prefix, scale=40.0, 
                             translateTo=jnt, rotateTo=jnt, 
                             parent="", shape='circleY', lockChannels=[])
             
@@ -228,7 +264,7 @@ class ControlAutorig(QtWidgets.QDialog):
 
     def controllerHierarchy(self, parent_of_jnt, ctrlOffsetName):
         if parent_of_jnt == '':
-            pass
+            print('controllerHierarchy"s parent_of_jnt is empty')
         else:
             print(parent_of_jnt)
             parent_of_jnt_prefix = parent_of_jnt.split(self.prefix)[-1]    
@@ -238,24 +274,33 @@ class ControlAutorig(QtWidgets.QDialog):
         mc.parentConstraint(ctrl_name, jnt, maintainOffset=True)
   
 #---------------------------------------------------------------------------------#
+#                        Maya Shelf Script
+#---------------------------------------------------------------------------------#
+def main():
+    path_ui = 'D:/ControlAutorig/04_ui/ui/ControlAutorig.ui'    # This path needs to be changed based on where the script is placed
+    try:
+        classVar.deleteLater()
+        classVar = None
+    except:
+        pass
+    
+    classVar = ControlAutorig(path_ui)
+    return classVar
+
+#---------------------------------------------------------------------------------#
 #                 CALLING THE CONTROL AUTORIG TOOL
 #---------------------------------------------------------------------------------#
 if __name__ == "__main__":
     try:
+        # If the script is copy-pasted in Maya and executed
         import maya.cmds as mc
-        # Maya
-        path_ui = 'D:/ControlAutorig/04_ui/ui/ControlAutorig.ui'    # This path needs to be changed based on where the script is placed
-        try:
-            classVar.deleteLater()
-            classVar = None
-        except:
-            pass
-        
-        classVar = ControlAutorig()
-    except ImportError: 
+        classVariable = main()
+    except ImportError: # There has to be a better way since I shouldn't rely on an Error
         # OS : Although this script won't function outside Maya. It is there just to see the UI.
         import sys
         app = QtWidgets.QApplication(sys.argv)
         path_ui = ("/").join([os.path.dirname(__file__), "ui", "ControlAutorig.ui"])
-        classVarOS = ControlAutorig()
+        classVarOS = ControlAutorig(path_ui)
         app.exec_()
+else:
+    import maya.cmds as mc
